fc37277a81148d9e74e7491f4c90b0d6f58c1df4
PavalaPa <Valentinka.pazyuk@mail.ru>
2025-05-15 17:36:50 +0300

    Add elements on PopulationForm

--- PopulationForm.Designer.cs
+++ PopulationForm.Designer.cs
@@ -26,19 +26,127 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.PopylationGrid = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.labelMaxReductionPopulation = new System.Windows.Forms.Label();
+            this.labelMinReductionPopulation = new System.Windows.Forms.Label();
+            this.buttonPredictPopulation = new System.Windows.Forms.Button();
+            this.numericUpDownPredict = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.PopylationGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPredict)).BeginInit();
             this.SuspendLayout();
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.985074F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOpenFile.Location = new System.Drawing.Point(30, 32);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(145, 36);
+            this.buttonOpenFile.TabIndex = 0;
+            this.buttonOpenFile.Text = "Открыть файл";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            // 
+            // PopylationGrid
+            // 
+            this.PopylationGrid.BackgroundColor = System.Drawing.Color.Azure;
+            this.PopylationGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.PopylationGrid.Location = new System.Drawing.Point(30, 74);
+            this.PopylationGrid.Name = "PopylationGrid";
+            this.PopylationGrid.RowHeadersWidth = 57;
+            this.PopylationGrid.RowTemplate.Height = 24;
+            this.PopylationGrid.Size = new System.Drawing.Size(346, 301);
+            this.PopylationGrid.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(415, 75);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(493, 300);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // labelMaxReductionPopulation
+            // 
+            this.labelMaxReductionPopulation.AutoSize = true;
+            this.labelMaxReductionPopulation.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.985074F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMaxReductionPopulation.Location = new System.Drawing.Point(27, 394);
+            this.labelMaxReductionPopulation.Name = "labelMaxReductionPopulation";
+            this.labelMaxReductionPopulation.Size = new System.Drawing.Size(522, 20);
+            this.labelMaxReductionPopulation.TabIndex = 3;
+            this.labelMaxReductionPopulation.Text = "За поледние 15 лет численность снизилась больше всего в:";
+            // 
+            // labelMinReductionPopulation
+            // 
+            this.labelMinReductionPopulation.AutoSize = true;
+            this.labelMinReductionPopulation.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.985074F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMinReductionPopulation.Location = new System.Drawing.Point(27, 429);
+            this.labelMinReductionPopulation.Name = "labelMinReductionPopulation";
+            this.labelMinReductionPopulation.Size = new System.Drawing.Size(524, 20);
+            this.labelMinReductionPopulation.TabIndex = 4;
+            this.labelMinReductionPopulation.Text = "За поледние 15 лет численность снизилась меньше всего в:";
+            // 
+            // buttonPredictPopulation
+            // 
+            this.buttonPredictPopulation.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.985074F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPredictPopulation.Location = new System.Drawing.Point(415, 32);
+            this.buttonPredictPopulation.Name = "buttonPredictPopulation";
+            this.buttonPredictPopulation.Size = new System.Drawing.Size(184, 36);
+            this.buttonPredictPopulation.TabIndex = 5;
+            this.buttonPredictPopulation.Text = "Прогнозировать на";
+            this.buttonPredictPopulation.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDownPredict
+            // 
+            this.numericUpDownPredict.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.98507F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDownPredict.Location = new System.Drawing.Point(605, 33);
+            this.numericUpDownPredict.Name = "numericUpDownPredict";
+            this.numericUpDownPredict.Size = new System.Drawing.Size(110, 33);
+            this.numericUpDownPredict.TabIndex = 6;
             // 
             // PopulationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(836, 549);
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(960, 466);
+            this.Controls.Add(this.numericUpDownPredict);
+            this.Controls.Add(this.buttonPredictPopulation);
+            this.Controls.Add(this.labelMinReductionPopulation);
+            this.Controls.Add(this.labelMaxReductionPopulation);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.PopylationGrid);
+            this.Controls.Add(this.buttonOpenFile);
             this.Name = "PopulationForm";
             this.Text = "PopulationForm";
+            ((System.ComponentModel.ISupportInitialize)(this.PopylationGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPredict)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.DataGridView PopylationGrid;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label labelMaxReductionPopulation;
+        private System.Windows.Forms.Label labelMinReductionPopulation;
+        private System.Windows.Forms.Button buttonPredictPopulation;
+        private System.Windows.Forms.NumericUpDown numericUpDownPredict;
     }
 }
